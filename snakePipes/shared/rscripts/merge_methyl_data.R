1512f7f7a6c9c58ca99afc8286cddda63c18c752
Steffen Heyne <steffen.heyne@gmail.com>
2019-04-14 01:50:39 +0200

    fix

--- snakePipes/shared/rscripts/merge_methyl_data.R
+++ snakePipes/shared/rscripts/merge_methyl_data.R
@@ -15,19 +15,24 @@ all = data_frame("chr"=character(0),"start"=integer(0),"end"=integer(0))
  
 for (f in files){
  dat=read.table(f,stringsAsFactors = F)
+ 
  name = gsub(".*/(.*).Percent_Methylation_by_Region.tsv",replacement = "\\1",f)
  print(name)
  colnames(dat) = c("chr","start","end",name)
  all=merge(all,dat,all=T)
 }
-
+mmpr[mmpr=="."]<-NA
 write.table(format(all[order(all[,1],all[,2]),], digits=2, scientific=F),file = outFile, quote = F,row.names = F,col.names = T,sep = "\t")
+
+################
 
 dat = read.table(paste0(FilePath,"/on_target_stats.per_region.mapq20.tsv"),header = T,comment.char = "")
 dat_mod = apply(dat[,-c(1:3)],2,function(x){x/sum(x)})*100
 final= cbind(dat[,1:3],dat_mod)
 
 write.table(format(final, digits=2, scientific=F),file = "custom_stats/on_target_stats.per_region.mapq20.perc.tsv", quote = F,row.names = F,col.names = T,sep = "\t")
+
+###############
 
 dat = read.table(paste0(FilePath,"/on_target_stats.per_region.tsv"),header = T,comment.char = "")
 dat_mod = apply(dat[,-c(1:3)],2,function(x){x/sum(x)})*100
@@ -41,11 +46,13 @@ write.table(format(final, digits=2, scientific=F),file = "custom_stats/on_target
 tcpgs = read.table(paste0(FilePath,"targets.CpG.bed"),header = F)
 
 colnames(tcpgs) = c("chr","start","end","name","score","strand","context")
-tcpgs$end = tcpgs$end+1
+#tcpgs$end = tcpgs$end+1
   
 files = Sys.glob(paste0(FilePath,"/methyl_calls/*_CpG.bedGraph"))
 
-all = data_frame("chr"=character(0),"start"=integer(0),"end"=integer(0))
+#all = data_frame("chr"=character(0),"start"=integer(0),"end"=integer(0))
+
+all = tcpgs[,c("chr","start","end")] 
 
 for (f in files){
   dat=read.table(f,stringsAsFactors = F,skip = 1)
